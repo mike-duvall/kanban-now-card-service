50613924fc730c2b0a6d9318ed22e9a910325109
Mike DuVall <mike.a.duvall@gmail.com>
2013-06-10 08:26:40 -0500

    Re-added getDatabase() to fix compile error.  Updated kill service to not use -9

--- src/main/java/kanbannow/CardServiceConfiguration.java
+++ src/main/java/kanbannow/CardServiceConfiguration.java
@@ -2,24 +2,21 @@ package kanbannow;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yammer.dropwizard.config.Configuration;
-import com.yammer.dropwizard.config.LoggingConfiguration;
-import com.yammer.dropwizard.config.RequestLogConfiguration;
 import com.yammer.dropwizard.db.DatabaseConfiguration;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 public class CardServiceConfiguration extends Configuration {
 
+    @Valid
+    @NotNull
+    @JsonProperty
+    private DatabaseConfiguration database = new DatabaseConfiguration();
 
-//    @Valid
-//    @NotNull
-//    @JsonProperty
-//    private DatabaseConfiguration database = new DatabaseConfiguration();
-//
-//
-//    public DatabaseConfiguration getDatabase() {
-//        return database;
-//    }
+
+    public DatabaseConfiguration getDatabase() {
+        return database;
+    }
 //
 //
 ////    @Valid
