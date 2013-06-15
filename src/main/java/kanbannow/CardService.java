be63fe2c1c8f953366b44375f7aa273b786fc68c
Mike DuVall <mike.a.duvall@gmail.com>
2013-06-15 15:31:30 -0500

    Added graphite reporter

--- src/main/java/kanbannow/CardService.java
+++ src/main/java/kanbannow/CardService.java
@@ -1,11 +1,14 @@
 package kanbannow;
 
+import com.yammer.metrics.reporting.GraphiteReporter;
 import kanbannow.health.TemplateHealthCheck;
 import kanbannow.resources.CardResource;
 
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+
+import java.util.concurrent.TimeUnit;
 
 public class CardService extends Service<CardServiceConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -22,6 +25,8 @@ public class CardService extends Service<CardServiceConfiguration> {
     public void run(CardServiceConfiguration configuration, Environment environment) throws Exception {
         environment.addResource(new CardResource( configuration.getDatabase()));
         environment.addHealthCheck(new TemplateHealthCheck("Make this a real healthCheck"));
+
+        GraphiteReporter.enable(15, TimeUnit.SECONDS, "carbon.hostedgraphite.com", 2003, "0cb986a9-f3e9-4292-8d08-0d3a759e448f");
 
     }
 
