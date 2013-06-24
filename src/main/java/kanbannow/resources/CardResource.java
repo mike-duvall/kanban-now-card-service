a307537cf45078fddb3514a6e398d1e09360e5ce
Mike DuVall <mike.a.duvall@gmail.com>
2013-06-24 16:22:59 -0500

    Fixed checkstyle violations

--- src/main/java/kanbannow/resources/CardResource.java
+++ src/main/java/kanbannow/resources/CardResource.java
@@ -1,7 +1,6 @@
 package kanbannow.resources;
 
 import kanbannow.core.Card;
-import com.google.common.base.Optional;
 import com.yammer.metrics.annotation.Timed;
 import kanbannow.jdbi.CardDAO;
 
@@ -10,7 +9,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.List;
