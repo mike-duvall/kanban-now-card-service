77d6032c8bf61edfc697c425eef082367feaa965
Mike DuVall <mike.a.duvall@gmail.com>
2013-06-30 12:37:00 -0500

    Commented out old test

--- src/test/java/kanbannow/CardServiceIntegrationTest.java
+++ src/test/java/kanbannow/CardServiceIntegrationTest.java
@@ -38,147 +38,154 @@ import static org.fest.assertions.Assertions.assertThat;
 public class CardServiceIntegrationTest {
 
 
-    public static final String PROPERTIES_PATH = "../properties/";
-    public static final String CARD_SERVICE_YML = "card-service.yml";
-    public static final String CARD_1_TEXT = "zzzTest card text1zzz";
-    public static final String CARD_2_TEXT = "zzzTest card text2zzz";
-    public static final String CARD_3_TEXT = "zzzTest card text3zzz";
-    public static final String CARD_4_TEXT = "zzzTest card text4zzz";
-    private Handle databaseHandle;
+//    public static final String PROPERTIES_PATH = "../properties/";
+//    public static final String CARD_SERVICE_YML = "card-service.yml";
+//    public static final String CARD_1_TEXT = "zzzTest card text1zzz";
+//    public static final String CARD_2_TEXT = "zzzTest card text2zzz";
+//    public static final String CARD_3_TEXT = "zzzTest card text3zzz";
+//    public static final String CARD_4_TEXT = "zzzTest card text4zzz";
+//    private Handle databaseHandle;
+//
+//
+//
+//    private DropwizardServiceRule<CardServiceConfiguration> serviceRule = new DropwizardServiceRule<CardServiceConfiguration>(CardService.class, PROPERTIES_PATH + CARD_SERVICE_YML);
+//
+//
+//    @Rule
+//    public DropwizardServiceRule<CardServiceConfiguration> getServiceRule() {
+//        return serviceRule;
+//    }
+//
+//
+//    @Before
+//    public void before() throws Exception {
+//        final DBIFactory factory = new DBIFactory();
+//        CardServiceConfiguration cardServiceConfiguration = serviceRule.getConfiguration();
+//        DatabaseConfiguration databaseConfiguration = cardServiceConfiguration.getDatabase();
+//        final DBI dbi = factory.build(serviceRule.getEnvironment(), databaseConfiguration, "oracle");
+//        cleanupDbData(dbi);
+//    }
+//
+//    // CHECKSTYLE:OFF
+//    private void cleanupDbData(DBI dbi) {
+//        databaseHandle = dbi.open();
+//        databaseHandle.execute("delete from card where text ='" + CARD_1_TEXT + "'");
+//        databaseHandle.execute("delete from card where text ='" + CARD_2_TEXT + "'");
+//        databaseHandle.execute("delete from card where text ='" + CARD_3_TEXT + "'");
+//        databaseHandle.execute("delete from card where text ='" + CARD_4_TEXT + "'");
+//    }
+//    // CHECKSTYLE:ON
+//
+//
+//    // CHECKSTYLE:OFF
+//    @Ignore
+//    @Test
+//    public void shouldReturnOnlyPostponedCardsFromCorrectBoardSortedByPostponedDate() throws Exception {
+//        Long boardId1 = 1L;
+//        Card card1 = createAndInsertPostponedCard(CARD_1_TEXT, "2/2/2101", boardId1);
+//        Card card2 = createAndInsertPostponedCard(CARD_2_TEXT, "1/1/2095", boardId1);
+//
+//        Long boardId2 = 2L;
+//        insertCardIntoBoard(boardId2, CARD_3_TEXT);
+//        insertCardIntoBoard(boardId2, CARD_4_TEXT);
+//        HttpResponse httpResponse = callCardService(boardId1);
+//        assertStatusCodeIs200(httpResponse);
+//        JsonNode actualJsonResults = getJsonResults(httpResponse);
+//        ArrayNode expectedJsonResults = createdExpectedJson(card1, card2);
+//        JSONAssert.assertEquals(  expectedJsonResults,  actualJsonResults );
+//    }
+//    // CHECKSTYLE:ON
+//
+//    private JsonNode getJsonResults(HttpResponse httpResponse) throws IOException {
+//        String result = getStringFromHttpResponse(httpResponse);
+//        ObjectMapper mapper = new ObjectMapper();
+//        return mapper.readTree( result );
+//    }
+//
+//    private ArrayNode createdExpectedJson(Card card1, Card card2) {
+//        JsonNodeFactory factory = JsonNodeFactory.instance;
+//        ArrayNode expectedCardArrayJson = new ArrayNode(factory);
+//        // Do card2 first, since it will be sorted as first item
+//        ObjectNode row = createObjectNodeFromCard(card2, factory);
+//        expectedCardArrayJson.add(row);
+//        row = createObjectNodeFromCard(card1, factory);
+//        expectedCardArrayJson.add(row);
+//        return expectedCardArrayJson;
+//    }
+//
+//    private Card createAndInsertPostponedCard(String text, String postponedDate, Long boardId) {
+//        Card card1 = new Card();
+//        card1.setCardText(text);
+//        card1.setPostponedDate(postponedDate);
+//        DateTimeFormatter formatter = DateTimeFormat.forPattern("MM/dd/yyyy");
+//        DateTime postponedDate1 = formatter.parseDateTime(card1.getPostponedDate());
+//        Long cardId1 = insertPostponedCardIntoBoard(boardId, card1, new Date(postponedDate1.getMillis()));
+//        card1.setId(cardId1);
+//        return card1;
+//    }
+//
+//    private String getStringFromHttpResponse(HttpResponse httpResponse) throws IOException {
+//        InputStream inputStream = httpResponse.getEntity().getContent();
+//        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+//        return bufferedReader.readLine();
+//    }
+//
+//    private void assertStatusCodeIs200(HttpResponse httpResponse) {
+//        StatusLine statusLine = httpResponse.getStatusLine();
+//        int statusCode = statusLine.getStatusCode();
+//        assertThat(statusCode).isEqualTo(200);
+//    }
+//
+//    //            String uri = "http://localhost:9595/cards/board/" + boardId + "?postponed=true";
+//    private HttpResponse callCardService(Long boardId1) throws IOException, ConfigurationException {
+//        CardServiceConfiguration configuration = serviceRule.getConfiguration();
+//        int port  = configuration.getHttpConfiguration().getPort();
+//        HttpClient httpclient = new DefaultHttpClient();
+//        String uri = "http://localhost:" + port + "/cards/board/" + boardId1;
+//        HttpGet httpget = new HttpGet(uri);
+//        return httpclient.execute(httpget);
+//    }
+//
+//
+//    private ObjectNode createObjectNodeFromCard(Card card, JsonNodeFactory factory) {
+//        ObjectNode row = new ObjectNode(factory);
+//        Long cardIdLong = card.getId();
+//        row.put("id", cardIdLong.intValue() );
+//        row.put("cardText", card.getCardText() );
+//        row.put("postponedDate", card.getPostponedDate() );
+//        return row;
+//    }
+//
+//
+//    private Long insertCardIntoBoard(Long boardId, String cardText) {
+//        long cardLocation = 1;
+//        Long cardId = getNextCardIdFromSequence();
+//
+//        databaseHandle.execute("insert into card (id, text, location, board_id) values (?, ?, ?, ?)", cardId, cardText, cardLocation, boardId);
+//
+//        return cardId;
+//    }
+//
+//    private Long getNextCardIdFromSequence() {
+//        return databaseHandle.createQuery("select CARD_SURROGATE_KEY_SEQUENCE.nextval from dual")
+//                    .map(LongMapper.FIRST)
+//                    .first();
+//    }
+//
+//    private Long insertPostponedCardIntoBoard(Long boardId, Card aCard, Date postponedDate) {
+//        long cardLocation = 1;
+//        Long cardId = getNextCardIdFromSequence();
+//
+//        databaseHandle.execute("insert into card (id, text, location, board_id, postponed_date) values (?, ?, ?, ?, ?)", cardId, aCard.getCardText(), cardLocation, boardId, postponedDate);
+//
+//        return cardId;
+//    }
 
 
-
-    private DropwizardServiceRule<CardServiceConfiguration> serviceRule = new DropwizardServiceRule<CardServiceConfiguration>(CardService.class, PROPERTIES_PATH + CARD_SERVICE_YML);
-
-
-    @Rule
-    public DropwizardServiceRule<CardServiceConfiguration> getServiceRule() {
-        return serviceRule;
-    }
-
-
-    @Before
-    public void before() throws Exception {
-        final DBIFactory factory = new DBIFactory();
-        CardServiceConfiguration cardServiceConfiguration = serviceRule.getConfiguration();
-        DatabaseConfiguration databaseConfiguration = cardServiceConfiguration.getDatabase();
-        final DBI dbi = factory.build(serviceRule.getEnvironment(), databaseConfiguration, "oracle");
-        cleanupDbData(dbi);
-    }
-
-    // CHECKSTYLE:OFF
-    private void cleanupDbData(DBI dbi) {
-        databaseHandle = dbi.open();
-        databaseHandle.execute("delete from card where text ='" + CARD_1_TEXT + "'");
-        databaseHandle.execute("delete from card where text ='" + CARD_2_TEXT + "'");
-        databaseHandle.execute("delete from card where text ='" + CARD_3_TEXT + "'");
-        databaseHandle.execute("delete from card where text ='" + CARD_4_TEXT + "'");
-    }
-    // CHECKSTYLE:ON
-
-
-    // CHECKSTYLE:OFF
     @Test
-    public void shouldReturnOnlyPostponedCardsFromCorrectBoardSortedByPostponedDate() throws Exception {
-        Long boardId1 = 1L;
-        Card card1 = createAndInsertPostponedCard(CARD_1_TEXT, "2/2/2101", boardId1);
-        Card card2 = createAndInsertPostponedCard(CARD_2_TEXT, "1/1/2095", boardId1);
+    public void dummy() {
 
-        Long boardId2 = 2L;
-        insertCardIntoBoard(boardId2, CARD_3_TEXT);
-        insertCardIntoBoard(boardId2, CARD_4_TEXT);
-        HttpResponse httpResponse = callCardService(boardId1);
-        assertStatusCodeIs200(httpResponse);
-        JsonNode actualJsonResults = getJsonResults(httpResponse);
-        ArrayNode expectedJsonResults = createdExpectedJson(card1, card2);
-        JSONAssert.assertEquals(  expectedJsonResults,  actualJsonResults );
-    }
-    // CHECKSTYLE:ON
-
-    private JsonNode getJsonResults(HttpResponse httpResponse) throws IOException {
-        String result = getStringFromHttpResponse(httpResponse);
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readTree( result );
-    }
-
-    private ArrayNode createdExpectedJson(Card card1, Card card2) {
-        JsonNodeFactory factory = JsonNodeFactory.instance;
-        ArrayNode expectedCardArrayJson = new ArrayNode(factory);
-        // Do card2 first, since it will be sorted as first item
-        ObjectNode row = createObjectNodeFromCard(card2, factory);
-        expectedCardArrayJson.add(row);
-        row = createObjectNodeFromCard(card1, factory);
-        expectedCardArrayJson.add(row);
-        return expectedCardArrayJson;
-    }
-
-    private Card createAndInsertPostponedCard(String text, String postponedDate, Long boardId) {
-        Card card1 = new Card();
-        card1.setCardText(text);
-        card1.setPostponedDate(postponedDate);
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("MM/dd/yyyy");
-        DateTime postponedDate1 = formatter.parseDateTime(card1.getPostponedDate());
-        Long cardId1 = insertPostponedCardIntoBoard(boardId, card1, new Date(postponedDate1.getMillis()));
-        card1.setId(cardId1);
-        return card1;
-    }
-
-    private String getStringFromHttpResponse(HttpResponse httpResponse) throws IOException {
-        InputStream inputStream = httpResponse.getEntity().getContent();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        return bufferedReader.readLine();
-    }
-
-    private void assertStatusCodeIs200(HttpResponse httpResponse) {
-        StatusLine statusLine = httpResponse.getStatusLine();
-        int statusCode = statusLine.getStatusCode();
-        assertThat(statusCode).isEqualTo(200);
-    }
-
-    //            String uri = "http://localhost:9595/cards/board/" + boardId + "?postponed=true";
-    private HttpResponse callCardService(Long boardId1) throws IOException, ConfigurationException {
-        CardServiceConfiguration configuration = serviceRule.getConfiguration();
-        int port  = configuration.getHttpConfiguration().getPort();
-        HttpClient httpclient = new DefaultHttpClient();
-        String uri = "http://localhost:" + port + "/cards/board/" + boardId1;
-        HttpGet httpget = new HttpGet(uri);
-        return httpclient.execute(httpget);
-    }
-
-
-    private ObjectNode createObjectNodeFromCard(Card card, JsonNodeFactory factory) {
-        ObjectNode row = new ObjectNode(factory);
-        Long cardIdLong = card.getId();
-        row.put("id", cardIdLong.intValue() );
-        row.put("cardText", card.getCardText() );
-        row.put("postponedDate", card.getPostponedDate() );
-        return row;
-    }
-
-
-    private Long insertCardIntoBoard(Long boardId, String cardText) {
-        long cardLocation = 1;
-        Long cardId = getNextCardIdFromSequence();
-
-        databaseHandle.execute("insert into card (id, text, location, board_id) values (?, ?, ?, ?)", cardId, cardText, cardLocation, boardId);
-
-        return cardId;
-    }
-
-    private Long getNextCardIdFromSequence() {
-        return databaseHandle.createQuery("select CARD_SURROGATE_KEY_SEQUENCE.nextval from dual")
-                    .map(LongMapper.FIRST)
-                    .first();
-    }
-
-    private Long insertPostponedCardIntoBoard(Long boardId, Card aCard, Date postponedDate) {
-        long cardLocation = 1;
-        Long cardId = getNextCardIdFromSequence();
-
-        databaseHandle.execute("insert into card (id, text, location, board_id, postponed_date) values (?, ?, ?, ?, ?)", cardId, aCard.getCardText(), cardLocation, boardId, postponedDate);
-
-        return cardId;
     }
 
 
